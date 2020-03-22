bbec3d5bda95dd530cb747eec83570f0d6f94230
C4rL05cc <60831643+C4rL05cc@users.noreply.github.com>
2020-03-21 20:37:02 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,38 +8,14 @@ import { FacebookLoginButton } from 'react-social-login-buttons';
 import xls1 from '../src/files/Reporte-Pruebas-PRC01.2.xlsx';
 import xls2 from '../src/files/Reporte-Pruebas-APS01.xlsx';
 import xls3 from '../src/files/Reporte-Pruebas-MRW01.2.xlsx';
-
+import xls4 from '../src/files/base de datos-mysql.xlsx';
+import doc1 from '../src/files/Reporte-Inventario.docx';
 
 class App extends Component {
   render (){
     return (
     <div>
-      <Form className="login-form">
-        <h1>Sistema <span className="font-weight-bold">CUPS</span></h1>
-        <h2 className="text-center">Bienvenido</h2>
-        <FormGroup>
-          <Label>Correo electrónico</Label><br></br>
-          <input type="email" placeholder="e-mail" />
-        </FormGroup>
-        <FormGroup>
-          <Label>Contraseña</Label><br></br>
-          <input type="password" placeholder="password" /><br></br>
-        </FormGroup>
-        <button className="btn-lg btn-dark btn-block">
-           Enviar
-        </button>
-        <div className="text-center pt-3">
-          O inicie sesión con sus redes sociales
-        </div>
-        <FacebookLoginButton className="mt-3 mb-3"/>
-        <div className="text-center pt-3"></div>
-          <a href="/signup">Registrarse </a>
-          <span className="p-2">|</span>
-          <a href="/signup"> Olvidé mi contraseña</a>
-         
-      </Form>
-      <br></br><br></br>
-      <hr></hr>
+      
       <h1 className="centro">Avances</h1>
       <h2 className="centro">
         ADMINISTRAR PRODUCTOS Y SERVICIOS #4
@@ -68,6 +44,48 @@ class App extends Component {
       <p className="centro">Reporte de pruebas del Diseño de interfaz de modificacion de reservaciones del empleado 
         <a href={xls3}> (MRW01.2)</a></p>
         <br></br><br></br><br></br><br></br>
+        <hr></hr><br></br>
+        <h1 className="centro">Avances Sprint 3</h1>
+        <h2 className="centro">Se decide modificar la interfaz del cliente, mostrando informacion concerniente a la contingencia</h2>
+        <img src={require('./imgs/Update-cliente.png')} className="imagce"/>
+
+        <h2 className="centro">Inventario de los productos y servicios disponibles APS01.1</h2>
+        <img src={require('./imgs/inventario.png')} className="imagce"/><br></br>
+        <h2 className="centro">Base de datos Mysql PRC01.3</h2>
+        <img src={require('./imgs/base.png')} className="imagce"/>
+
+        <p className="centro">Estructura de la base de datos Mysql 
+        <a href={xls4}> (PRC01.3)</a></p>
+        <p className="centro">Reporte de inventario de productos y servicios 
+        <a href={doc1}> (APS01.1)</a></p>
+        <br></br><br></br>
+        <h1 className="centro">Login al sistema CUPS MRW01.3</h1>
+        <Form className="login-form">
+        <h1>Sistema <span className="font-weight-bold">CUPS</span></h1>
+        <h2 className="text-center">Bienvenido</h2>
+        <FormGroup>
+          <Label>Correo electrónico</Label><br></br>
+          <input type="email" placeholder="e-mail" />
+        </FormGroup>
+        <FormGroup>
+          <Label>Contraseña</Label><br></br>
+          <input type="password" placeholder="password" /><br></br>
+        </FormGroup>
+        <button className="btn-lg btn-dark btn-block">
+           Enviar
+        </button>
+        <div className="text-center pt-3">
+          O inicie sesión con sus redes sociales
+        </div>
+        <FacebookLoginButton className="mt-3 mb-3"/>
+        <div className="text-center pt-3"></div>
+          <a href="/signup">Registrarse </a>
+          <span className="p-2">|</span>
+          <a href="/signup"> Olvidé mi contraseña</a>
+         
+      </Form>
+      <br></br><br></br>
+      <hr></hr>
     </div>
     );
   }
